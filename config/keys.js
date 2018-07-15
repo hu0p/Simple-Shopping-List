74c79b5ee5559e4292437e27552a10f4eea539ea
Logan Houp <logan@webdogs.com>
2018-07-15 12:30:46 -0700

    add initial server and database config

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb://huop:ClaQuochHakFom1@ds249545.mlab.com:49545/shopping_list_hu0p"
+};
---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+// middleware
+app.use(bodyParser.json());
+
+// DB config
+
+const db = require("./config/keys.js").mongoURI;
+
+// 🔌
+
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB Connected Successfully."))
+  .catch(error => console.log(error));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server initialized on port ${port}`));
