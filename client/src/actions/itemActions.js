3102d4de85bd4ef2a0ff0bdc94e4677682a42766
Logan Houp <17230438+hu0p@users.noreply.github.com>
2018-07-26 16:48:42 -0700

    Finalize connection to backend

--- client/src/actions/itemActions.js
+++ client/src/actions/itemActions.js
@@ -3,26 +3,33 @@ import { GET_ITEMS, ADD_ITEM, DELETE_ITEM, ITEMS_LOADING } from "./types";
 
 export const getItems = () => dispatch => {
   dispatch(setItemsLoading());
-  axios.get("/api/items/").then(res =>
+  axios
+    .get("/api/items")
+    .then(res =>
+      dispatch({
+        type: GET_ITEMS,
+        payload: res.data
+      })
+    )
+    .catch(err => console.log(err));
+};
+
+export const addItem = item => dispatch => {
+  axios.post("/api/items", item).then(res =>
     dispatch({
-      type: GET_ITEMS,
+      type: ADD_ITEM,
       payload: res.data
     })
   );
 };
 
-export const deleteItem = id => {
-  return {
-    type: DELETE_ITEM,
-    payload: id
-  };
-};
-
-export const addItem = item => {
-  return {
-    type: ADD_ITEM,
-    payload: item
-  };
+export const deleteItem = id => dispatch => {
+  axios.delete(`/api/items/${id}`).then(res =>
+    dispatch({
+      type: DELETE_ITEM,
+      payload: id
+    })
+  );
 };
 
 export const setItemsLoading = () => {
