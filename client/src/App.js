7150e1df75130758f5ce390d153c2df62627709f
Logan Houp <17230438+hu0p@users.noreply.github.com>
2018-07-26 12:04:48 -0700

    add modal component

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import AppNavBar from "./components/AppNavBar";
 import ShoppingList from "./components/ShoppingList";
+import ItemModal from "./components/ItemModal";
+import { Container } from "reactstrap";
 
 import { Provider } from "react-redux";
 import store from "./store";
@@ -8,14 +10,16 @@ import store from "./store";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
-
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
         <div className="App">
           <AppNavBar />
-          <ShoppingList />
+          <Container>
+            <ItemModal />
+            <ShoppingList />
+          </Container>
         </div>
       </Provider>
     );
