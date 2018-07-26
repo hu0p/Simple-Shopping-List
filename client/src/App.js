eab1cae3486a59ea85308474aab6b8760f525951
Logan Houp <logan@webdogs.com>
2018-07-25 18:56:53 -0700

    add redux boilerplate

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,23 @@
 import React, { Component } from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
 import AppNavBar from "./components/AppNavBar";
 import ShoppingList from "./components/ShoppingList";
+
+import { Provider } from "react-redux";
+import store from "./store";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <AppNavBar />
-        <ShoppingList />
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <AppNavBar />
+          <ShoppingList />
+        </div>
+      </Provider>
     );
   }
 }
